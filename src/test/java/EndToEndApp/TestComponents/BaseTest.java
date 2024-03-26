52ec3c080d7542088177494a7276d9b35ad3667d
ananya-m-106 <ramusfkacc1@gmail.com>
2024-03-26 11:49:59 +0530

    Added Edge browser initialization

--- src/test/java/EndToEndApp/TestComponents/BaseTest.java
+++ src/test/java/EndToEndApp/TestComponents/BaseTest.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -45,6 +46,8 @@ public class BaseTest {
 			driver.manage().window().setSize(new Dimension(1440,900));
 		}else if(browserName.equalsIgnoreCase("firefox")) {
 			driver = new FirefoxDriver();
+		}else if(browserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
 		}
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
